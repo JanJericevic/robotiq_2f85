0564e97ae61cb8f348004c27854d99f30c80fbb9
Jan Jericevic <jan.jericevic@gmail.com>
2024-03-05 17:59:59 +0100

    added gripper joint states publisher

---
+++ robotiq_2f_gripper_control/nodes/gripper_joint_state_publisher.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import rospy
+import robotiq_2f_gripper_control.msg as gripper_msg
+from sensor_msgs.msg import JointState
+
+
+class GripperJointPublisher:
+    def __init__(self, prefix):
+        self.prefix = prefix
+        self.pub = rospy.Publisher('gripper_joint_states', JointState, queue_size=10)
+        self.sub = rospy.Subscriber('input', gripper_msg.Robotiq2FGripper_robot_input, self.callback)
+
+    def callback(self, data):
+        rospy.loginfo('gripper in position: %i' % data.gPO)
+        state = JointState()
+        state.header.stamp = rospy.Time.now()
+        state.name = [self.prefix + 'finger_joint']
+        # gPO = 0 when open, 255 when closed
+        # srdf: gripper_finger_joint = 0 when open, 0.755 when closed
+        # robotiq manual: quasi-linear relationship between gPO and joint state
+        state.position = [float(data.gPO) / 255.0 * 0.755]
+        # state.position = [(1 - (float(data.gPO) / 255.0)) * 0.755]
+        state.velocity = [0.0]
+        state.effort = [0.0]
+
+        self.pub.publish(state)
+
+
+if __name__ == '__main__':
+    rospy.init_node('robotiq_2f_85_joint_state_publisher')
+    prefix = rospy.get_param('~prefix')
+    gj = GripperJointPublisher(prefix)
+
+    rospy.spin()
